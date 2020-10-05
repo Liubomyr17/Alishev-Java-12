3c25b81950a65c0d49d409ec4119e32d525be630
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-05 23:46:02 +0300

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,35 @@
+package com.company;
+
+
+public class Solution {
+    public static void main(String[] args){
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+       // System.out.println(numbers[0]);
+       // System.out.println(numbers[1]);
+        // Output 10 0
+        String[] strings = new String[3];
+        strings[0] = "hello";
+        strings[1] = "bye";
+        strings[2] = "java";
+        // System.out.println(strings[0]);
+        // System.out.println(strings[2]);
+
+        for (int i=0; i<strings.length; i++) {
+            System.out.println(strings[i]);
+        }
+        System.out.println();
+        for(String string : strings) {
+            System.out.println(string);
+        }
+        int[] numbers1 = {1,2,3};
+        int sum = 0;
+        for(int x : numbers1) {
+            sum = sum+x;
+        }
+        System.out.println();
+        System.out.println(sum);
+
+        int value = 0;
+    }
+}
